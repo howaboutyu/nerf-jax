17cee66b184930243ca3a6bc49c2eb6ef9e963a8
higgsboost <higgsboost@gmail.com>
2022-10-25 21:51:09 +1300

    fixed not updating bug

--- train.py
+++ train.py
@@ -25,7 +25,6 @@ dataset = dataset_factory(config)
 nerf_components = get_nerf_componets(config)
 
 state = nerf_components['state']
-params = nerf_components['state'].params
 grad_fn = nerf_components['grad_fn']
 render_fn = nerf_components['render_eval_fn']
 model_fn = nerf_components['model']
@@ -34,13 +33,13 @@ key = jax.random.PRNGKey(0)
 
 
 @jax.jit
-def train_step(params, data, state):
-    loss_val, grads, pred_train = jax.pmap(grad_fn, in_axes=(None, 0))(params, data)
+def train_step(data, state):
+    loss_val, grads, pred_train = jax.pmap(grad_fn, in_axes=(None, 0))(state.params, data)
 
     loss_val = jnp.mean(loss_val)
     grads = jax.tree_map(lambda x : jnp.mean(x, 0), grads)
     state = state.apply_gradients(grads=grads)
-    return params, state, loss_val, pred_train
+    return state, loss_val, pred_train
 
     
 for i in range(config['num_epochs']):
@@ -54,7 +53,7 @@ for i in range(config['num_epochs']):
             key_train = random.split(key, len(key_train) * dataset['train'].batch_size).reshape((-1, img.shape[-4], 2))
 
         data = (origins, directions, img, key_train)
-        params, state, loss_val, pred_train = train_step(params, data, state)
+        state, loss_val, pred_train = train_step(data, state)
         
         if config['split_to_patches']:
             if config['use_batch']: pred_train = pred_train[0] # just take first example
