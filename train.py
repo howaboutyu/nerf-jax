f7b55fd0bf5b99f890b02052b457b0daf858e1bd
higgsboost <higgsboost@gmail.com>
2022-11-18 18:57:33 +1300

    added flags and render step

--- train.py
+++ train.py
@@ -1,60 +1,44 @@
-from flax import linen as nn
-from flax.training import train_state, checkpoints
 
 import jax
 from jax import grad, jit, vmap
+from flax.training import checkpoints
 import jax.numpy as jnp
 import jax.random as random
-import numpy as np
-import optax
 
+import numpy as np
 import cv2
 import yaml
+from absl import app, flags
 
 from nerf import get_nerf_componets
 from datasets import dataset_factory
 
-# TODO: add input args 
-with open('configs/beer.yaml') as file:
-  config = yaml.safe_load(file)
-
-ckpt_dir = config['ckpt_dir'] 
-print('-----------------')
-dataset = dataset_factory(config) 
-
-config.update({'near': dataset['train'].near, 'far': dataset['train'].far})
-nerf_components = get_nerf_componets(config)
-
-state = nerf_components['state']
-grad_fn = nerf_components['grad_fn']
-model_fn = nerf_components['model']
-render_fn = nerf_components['render_fn']
-render_hvs_fn = nerf_components['render_hvs_fn']
+FLAGS = flags.FLAGS
+flags.DEFINE_string("config_path", None, "config file path")
+flags.DEFINE_string("mode", "train", "mode; can be train or render")
+flags.mark_flag_as_required("config_path")
 
 
-key = jax.random.PRNGKey(0)
-
-print(f'Hello here are your jax devices: {jax.devices()}')
-
-@jit
-def train_step(data, state):
+def train_step(data, state, grad_fn):
     loss_val, grads, pred_train, weights, ts = jax.pmap(grad_fn, in_axes=(None, 0))(state.params, data)
     loss_val = jnp.mean(loss_val)
     grads = jax.tree_map(lambda x : jnp.mean(x, 0), grads)
     state = state.apply_gradients(grads=grads)
     return state, loss_val, pred_train, weights, ts
 
-def val_step(data, state, H, W):
+def render_step(data, state, H, W, render_fn, render_hvs_fn):
 
     params = state.params 
     
-    (img, origins, directions) = data
+    (origins, directions) = data
     origins = origins.reshape((-1, 3))
     directions = directions.reshape((-1, 3))
-    img = img.reshape((-1, 3))
-    eval_bs = 4096//4 
+    eval_bs = 4096//2 
+
+    key = jax.random.PRNGKey(0)
+
     pred_imgs = []
-    for i in range(0, len(img), eval_bs):
+    for i in range(0, len(origins), eval_bs):
         jax.debug.print('eval step {}', i)
         _, weights, _ = render_fn(params, origins[i:i+eval_bs], directions[i:i+eval_bs], key)
         pred_img, _, _ = render_hvs_fn(params, origins[i:i+eval_bs], directions[i:i+eval_bs], key, weights)
@@ -64,22 +48,74 @@ def val_step(data, state, H, W):
     pred_img = jnp.reshape(pred_imgs, (H, W, 3))
     return pred_img
 
-for i in range(config['num_epochs']):
-    # train    
-    for idx, (img, origins, directions) in enumerate(dataset['train']):
+def main(argv):
+    del argv
 
-        key_train = random.split(key, img.shape[0])
+    with open(FLAGS.config_path) as file:
+      config = yaml.safe_load(file)
+    
+    ckpt_dir = config['ckpt_dir'] 
+    
+    # get dataset iterator
+    dataset = dataset_factory(config) 
+   
+    config.update({'near': dataset['train'].near, 'far': dataset['train'].far})
+    nerf_components = get_nerf_componets(config)
+    
+    state = nerf_components['state']
+    grad_fn = nerf_components['grad_fn']
+    model_fn = nerf_components['model']
+    render_fn = nerf_components['render_fn']
+    render_hvs_fn = nerf_components['render_hvs_fn']
+    
+    
+    key = jax.random.PRNGKey(0)
+    
+    print(f'jax devices: {jax.devices()}')
 
-        data = (origins, directions, img, key_train)
-        state, loss_val, pred_train, weights, ts = train_step(data, state)
-        print(f'Epoch {i} step {idx} loss : {loss_val}')
+    if FLAGS.mode == 'train':
+        for i in range(config['num_epochs']):
+            # train    
+            for idx, (img, origins, directions) in enumerate(dataset['train']):
         
-        key, _ = random.split(key)
+                key_train = random.split(key, img.shape[0])
+        
+                data = (origins, directions, img, key_train)
+                state, loss_val, pred_train, weights, ts = train_step(data, state)
+                print(f'Epoch {i} step {idx} loss : {loss_val}')
+                
+                key, _ = random.split(key)
+        
+            # val and save checkpoint
+            if i % 10 == 0 and i > 0:    
+                data = dataset['val'].get(0)
+                pred_img = render_step(
+                        (data[1], data[2]), 
+                        state, 
+                        dataset['train'].H, 
+                        dataset['train'].W,
+                        render_fn,
+                        render_hvs_fn,
+                        )
+                cv2.imwrite(f'/tmp/eval_{i}.jpg', np.array(pred_img * 255)) 
+                checkpoints.save_checkpoint(ckpt_dir=ckpt_dir, target=state, step=i, overwrite=True)
+    elif FLAGS.mode == 'render':
+        print('rendering started ...')
+        for idx, data in enumerate(dataset['render']):
+            pred_img = render_step(
+                        data, 
+                        state, 
+                        dataset['train'].H, 
+                        dataset['train'].W,
+                        render_fn,
+                        render_hvs_fn,
+                        )
 
-    # val
-    if i % 10 == 0 and i > 0:    
-        pred_img = val_step(dataset['val'].get(0), state, dataset['train'].H, dataset['train'].W)
-        cv2.imwrite(f'/tmp/eval_{i}.jpg', np.array(pred_img * 255))
+            cv2.imwrite(f'/tmp/{config["dataset_name"]}_render_' +str(idx).zfill(4)+ '.jpg', np.array(pred_img * 255))
 
-        checkpoints.save_checkpoint(ckpt_dir=ckpt_dir, target=state, step=i, overwrite=True)
+    else:
+        print('Mode can only be : [train, render]') 
 
+
+if __name__ == '__main__':
+    app.run(main)
