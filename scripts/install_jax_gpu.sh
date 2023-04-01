77e3b678811cc5062216c2b3503e4a6a86a43aa3
higgsboost <higgsboost@gmail.com>
2023-04-01 16:18:56 +1300

    pip install scripts for `jax`

--- scripts/install_jax_gpu.sh
+++ scripts/install_jax_gpu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e 
 # Setup Jax on GPU
-sudo apt update -y
 pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
-echo You have successfully install JAX 😁 on the GPU
+echo You have successfully install JAX 😁 on GPU
