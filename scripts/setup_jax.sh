7f2607f2471b0e68f96473269f61297470606ca8
higgsboost <higgsboost@gmail.com>
2023-04-01 15:31:25 +1300

    Move `opencv` deps to another script

--- scripts/setup_jax.sh
+++ scripts/setup_jax.sh
@@ -1,18 +1,7 @@
+#!/bin/bash
+set -e 
 # Setup Jax on GPU
 sudo apt update -y
 pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-
-# nerf 
-if [ -d "nerf" ]; then
-	git clone https://github.com/higgsboost/nerf-jax ~/nerf
-else
-	ehco nerf also exists
-fi
-
-cd ~/nerf
-mkdir llff_data
-sudo apt-get install ffmpeg libsm6 libxext6  -y
-
-pip install -r ~/nerf/requirements.txt
 
 echo You have successfully install JAX 😁 on the GPU
