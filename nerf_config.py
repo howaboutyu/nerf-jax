a9d2c30d57f63e982f5a3b757cdbcd63b3648644
higgsboost <higgsboost@gmail.com>
2023-04-01 13:46:19 +1300

    Added configs

--- nerf_config.py
+++ nerf_config.py
@@ -25,9 +25,10 @@ class NerfConfig:
     learning_rate: float = 0.0001
     optimizer: str = 'adam'
 
-
+    max_steps: int = 1e6
     steps_per_eval: int = 100 # <-- number of steps per evaluation
     steps_per_ckpt: int = 100 # <-- number of steps per checkpoint
+    log_every: int = 10 # <-- number of steps per log
     ckpt_dir: str = 'checkpoints' # <-- directory where checkpoints are saved
     load_ckpt_dir: str = None # <-- directory where checkpoints are loaded from
 
